8f60bfc8fb3c6388aa174fa742d5b4f14a7f1317
LovroG05 <lovro.govekar@gmail.com>
2021-02-18 08:27:26 +0100

    added cotton and quartz spikes

---
+++ src/main/java/tk/lovroindustries/extra_plants/item/ItemCotton.java
@@ -0,0 +1,60 @@
+
+package tk.lovroindustries.extra_plants.item;
+
+import tk.lovroindustries.extra_plants.ElementsExtraPlantsMod;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+@ElementsExtraPlantsMod.ModElement.Tag
+public class ItemCotton extends ElementsExtraPlantsMod.ModElement {
+	@GameRegistry.ObjectHolder("extra_plants:cotton")
+	public static final Item block = null;
+	public ItemCotton(ElementsExtraPlantsMod instance) {
+		super(instance, 6);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("extra_plants:cotton", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 64;
+			setUnlocalizedName("cotton");
+			setRegistryName("cotton");
+			setCreativeTab(CreativeTabs.MISC);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+	}
+}
---
+++ src/main/java/tk/lovroindustries/extra_plants/procedure/ProcedureOnCottonBonemealed.java
@@ -0,0 +1,78 @@
+package tk.lovroindustries.extra_plants.procedure;
+
+import tk.lovroindustries.extra_plants.block.BlockCottonBush;
+import tk.lovroindustries.extra_plants.ElementsExtraPlantsMod;
+
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.event.entity.player.BonemealEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.item.EntityItem;
+
+import java.util.Map;
+
+@ElementsExtraPlantsMod.ModElement.Tag
+public class ProcedureOnCottonBonemealed extends ElementsExtraPlantsMod.ModElement {
+	public ProcedureOnCottonBonemealed(ElementsExtraPlantsMod instance) {
+		super(instance, 8);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			System.err.println("Failed to load dependency x for procedure OnCottonBonemealed!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			System.err.println("Failed to load dependency y for procedure OnCottonBonemealed!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			System.err.println("Failed to load dependency z for procedure OnCottonBonemealed!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			System.err.println("Failed to load dependency world for procedure OnCottonBonemealed!");
+			return;
+		}
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		World world = (World) dependencies.get("world");
+		if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == BlockCottonBush.block.getDefaultState().getBlock())) {
+			if (!world.isRemote) {
+				EntityItem entityToSpawn = new EntityItem(world, x, y, z, new ItemStack(BlockCottonBush.block, (int) (1)));
+				entityToSpawn.setPickupDelay(10);
+				world.spawnEntity(entityToSpawn);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onBonemeal(BonemealEvent event) {
+		EntityPlayer entity = event.getEntityPlayer();
+		int i = event.getPos().getX();
+		int j = event.getPos().getY();
+		int k = event.getPos().getZ();
+		World world = event.getWorld();
+		ItemStack itemstack = event.getStack();
+		java.util.HashMap<String, Object> dependencies = new java.util.HashMap<>();
+		dependencies.put("x", i);
+		dependencies.put("y", j);
+		dependencies.put("z", k);
+		dependencies.put("world", world);
+		dependencies.put("itemstack", itemstack);
+		dependencies.put("entity", entity);
+		dependencies.put("event", event);
+		this.executeProcedure(dependencies);
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+}
